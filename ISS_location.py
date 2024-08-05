33c8b6777d57fa4c03b4d6912db8039d00ac014e
Michael <pth.a10.08.20132014@gmail.com>
2024-08-05 15:07:32 -0400

    Modify ISS_location.py

--- ISS_location.py
+++ ISS_location.py
@@ -10,15 +10,15 @@ def get_location():
     #If open notify is broken or wonky use the alternative url to run
 
     #Getting JSON API
-    # request = urllib.request.urlopen(ISS_URL)
-    request = urllib.request.urlopen(ISS_ALTERNATIVE_URL)
+    request = urllib.request.urlopen(ISS_URL)
+    # request = urllib.request.urlopen(ISS_ALTERNATIVE_URL)
     response = json.loads(request.read())
 
     #Get pos of coordinate
-    # lat = response['iss_position']['latitude']
-    # lon = response['iss_position']['longitude']
-    lat = response['latitude']
-    lon = response['longitude']   
+    lat = response['iss_position']['latitude']
+    lon = response['iss_position']['longitude']
+    # lat = response['latitude']
+    # lon = response['longitude']   
     return lat,lon
 
 print(get_location())
