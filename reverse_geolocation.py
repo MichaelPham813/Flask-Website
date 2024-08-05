164aacae7adc4a5cf95291476ae8994e9b1023f2
Michael <pth.a10.08.20132014@gmail.com>
2024-08-05 15:37:19 -0400

    Fixing reverse_geolocation variables name

--- reverse_geolocation.py
+++ reverse_geolocation.py
@@ -5,10 +5,11 @@ import os
 load_dotenv()
 
 def get_country(lat,lon):
-    API_KEY = os.getenv('BIG_CLOUD_DATA_API_KEY')
+    API_KEY = os.getenv('BIG_DATA_CLOUD_API_KEY')
     URL = f'https://api-bdc.net/data/reverse-geocode?latitude={lat}&longitude={lon}&localityLanguage=en&key={API_KEY}'
     
     request = urllib.request.urlopen(URL,timeout = 15)
     response = json.loads(request.read())
     
     return response
+
